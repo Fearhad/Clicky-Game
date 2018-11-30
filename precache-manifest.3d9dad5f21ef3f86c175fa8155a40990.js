8588023f3bef02553bcda9bd898140ab24f1bc80
Allan Hartigan <allan.hartigan@gmail.com>
2018-11-29 21:40:36 -0500

    Updates

---
+++ precache-manifest.3d9dad5f21ef3f86c175fa8155a40990.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e04d64db5df4031706a79b6cae8cc9a",
+    "url": "/Clicky-Game/static/media/aas.4e04d64d.JPG"
+  },
+  {
+    "revision": "ea079e327f2a45236d7a",
+    "url": "/Clicky-Game/static/css/main.b3e8ebce.chunk.css"
+  },
+  {
+    "revision": "c88d4453e1a9ea08ba25",
+    "url": "/Clicky-Game/static/js/1.c88d4453.chunk.js"
+  },
+  {
+    "revision": "2f9ed21177874c072eef",
+    "url": "/Clicky-Game/static/js/runtime~main.2f9ed211.js"
+  },
+  {
+    "revision": "58dc21d2824103c647c1d6d8a06be9ed",
+    "url": "/Clicky-Game/static/media/warcraft.58dc21d2.jpg"
+  },
+  {
+    "revision": "ea079e327f2a45236d7a",
+    "url": "/Clicky-Game/static/js/main.ea079e32.chunk.js"
+  },
+  {
+    "revision": "02c6a507e2c7ad9f518392f9adcf5e90",
+    "url": "/Clicky-Game/static/media/tile11.02c6a507.jpg"
+  },
+  {
+    "revision": "5d7a4364dfeb79551f23f06c9843dbaf",
+    "url": "/Clicky-Game/static/media/tile12.5d7a4364.jpg"
+  },
+  {
+    "revision": "1d052f8a78dbfa1b126ebb814406855f",
+    "url": "/Clicky-Game/static/media/tile7.1d052f8a.jpg"
+  },
+  {
+    "revision": "6119b79be62d264272b88ce144029831",
+    "url": "/Clicky-Game/static/media/warcraft.6119b79b.png"
+  },
+  {
+    "revision": "4d078a03d282b943f6d06e241a89b0bc",
+    "url": "/Clicky-Game/index.html"
+  }
+];
